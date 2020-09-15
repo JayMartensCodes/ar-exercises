b319f6a3374941e6ca4a19f61dc195eacfb22a7f
Jay Martens <martensjay93@gmail.com>
2020-09-15 01:21:02 -0400

    Updated the classes and added employees to the first two stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Cal", last_name: "Adryn", hourly_rate: 50)
+@store1.employees.create(first_name: "Jay", last_name: "Martens", hourly_rate: 100)
+@store2.employees.create(first_name: "Billy", last_name: "Smith", hourly_rate: 40)
+@store2.employees.create(first_name: "Joe", last_name: "Burrow", hourly_rate: 30)
+@store2.employees.create(first_name: "Tom", last_name: "Brady", hourly_rate: 12)
